b978f0a4bbedf2ae8b1f165011d35418e95202ae
Adneycm <adneycm@al.insper.edu.br>
2022-05-24 16:01:04 -0300

    Cria tabela com informações gerais do usuário

--- usersData/models.py
+++ usersData/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 
 class userData(models.Model):
+    newUser = models.BooleanField(default=True)
     username = models.CharField(max_length=200)
     cards = models.CharField(max_length=500, default="")
     money = models.IntegerField(default=0)
+    win = models.IntegerField(default=0)
+    defeat = models.IntegerField(default=0)
+    commonPacksOpened = models.IntegerField(default=0)
+    specialPacksOpened = models.IntegerField(default=0)
+    rarePacksOpened = models.IntegerField(default=0)
