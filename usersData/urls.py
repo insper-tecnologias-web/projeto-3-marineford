076d15d4e4b7dd351fdb000fb802e22379756203
matheus-1618 <matheus1207live@gmail.com>
2022-05-25 20:40:47 -0300

    Sobe rotas para users-data

---
+++ usersData/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+from . import views
+
+urlpatterns = [
+    path('usuarios', views.get_all_users), #(GET)Necessário para elaboração do ranking
+    path('usuarios/<str:username>', views.get_user), #(GET)Retornará informações referentes a apenas um usuário (nome,cartas, vitorias, moedas...)
+    path('after_battle', views.after_battle), #POST, permite registrar novas informações após uma batalha (se perdeu, venceu, ganhou ou perdeu moedas)
+    path('after_pack', views.after_pack),#POST, permite registrar após abertura de um pack, o valor agora de moedas, a nova carta adquirida...
+]
